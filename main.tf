f7776fcca50c7c58ca4edc789be67777ad5c8291
daivakrupa <daivathota2001@gmail.com>
2022-08-29 12:30:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "ec2service" {
+  source  = "app.terraform.io/Daivakrupa/ec2service/aws"
+  version = "1.0.1"
+
+  ami                       =  "ami-06489866022e12a14"
+  instance_type             =  "t2.micro"
+  key_name                  =  "terraform-kp.pem"
+  security_group_ids        =  ["sg-073bb75d9a1c74ce9"]
+  subnet_id                 =  "subnet-0c4c99f5bda8bd2af"
+  name                      =  "ec2service"
+  env                       =  "production"
+}
